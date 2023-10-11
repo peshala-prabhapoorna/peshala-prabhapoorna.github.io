f8b5b7142469bd38945c8146b53cede6502af4e3
Your Name <you@example.com>
2023-10-11 23:33:43 +0530

    site commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/matara_edited.png") {
+        myImage.setAttribute("src",
+        "images/logos.png");
+    } else {
+        myImage.setAttribute("src", "images/matara_edited.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
+
+function setUserName() {
+    let myName = prompt("Please enter your filthy name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `${myName}, are you looking to board in Matara?`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `${storedName}, are you looking to board in Matara?`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
